e0b3432f992e8a23dd51801db94696d26bd2944c
BiancaMesa <bianca.koraa@gmail.com>
2024-03-05 16:56:59 +0100

    search for series works

--- src/js/main.js
+++ src/js/main.js
@@ -3,61 +3,53 @@
 //Variables globales
 const inputSearch = document.querySelector('.js-input-search');
 const searchButton = document.querySelector('.js-search-btn');
-const seriesCard = document.querySelector('.js-search-list'); 
+const searchedSeriesContainer = document.querySelector('.js-search-container'); 
+const favSeriesContainer = document.querySelector('.js-fav-container'); 
 const url = 'https://api.jikan.moe/v4/anime?q='; 
-let APIUrlSeries = ''; 
-let seriesList = [];
+let searchResult = [];
+let seriesFound = [];
+let favList = []; 
 
 
-//Function find a series
+//Find series
 function findSeries () {
     const inputSearchValue = inputSearch.value; 
-    //const url = 'https://api.jikan.moe/v4/anime?q=' + inputSearchValue; 
-
-    //API request 
+    
+    //API request: la propia API va a devolver los resultados de la búsqueda
     fetch(url + inputSearchValue)
     .then (response => response.json())
     .then (data => {
-        seriesList = data;
-        // seriesList = data.data[2].title;
-        console.log(seriesList); 
+        searchResult = data.data;
+        printSeries();
     })
 }
 
-//Print TV shows 
-function printSeries(data) {
-    const seriesTitle = data.title; 
-    const imageUrl = data.images.jpg.image_url; 
+//Print series
+function printSeries() {
+    let seriesFound = ''; 
 
-    //Create the card with image and title of the series in HTML
-    seriesCard.innerHTML = `
-        <div>
-            <h2>${title}</h2>
-            <img src="${imageUrl}" alt="${title}">
-        </div>
-        `;
+    for (const series of searchResult) {
+        const seriesTitle = series.title; 
+        const seriesImage = series.images.jpg.image_url; 
 
+        seriesFound += `
+            <h3>${seriesTitle}</h3>
+            <img src="${seriesImage}" alt="${seriesTitle}">
+            `
+    }
+
+    searchedSeriesContainer.innerHTML = seriesFound; 
 }
 
 
-//Filter TV shows
-function handleSearchClick (event) {
+//Search handle function 
+function handleSearchClick(event) {
     event.preventDefault(); 
-    findSeries(); 
-    printSeries(); 
-    console.log('1'); 
-    // const inputSearchValue = inputSearch.value; 
-    //console.log(inputSearchValue); 
-
-    //array con las series filtradas
-    const filteredSeries = seriesList.filter(function (series) {
-        console.log('2'); 
-        return seriesListAPIUrlSeries = URL + '&search' + inputSearchValue; 
-        // return series.titles.toLowerCase().includes(inputSearchValue.toLowerCase()); 
-    }); 
-    
-    // printSeries (filteredSeries); 
-
+    findSeries();
 }
 
 searchButton.addEventListener('click', handleSearchClick);
+
+
+//PART 2 
+//Add to favorites handle function
